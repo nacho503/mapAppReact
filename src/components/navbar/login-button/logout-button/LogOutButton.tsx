6c69a4eca5d3dcc2400f0edd9b55b70903fe85ea
nacho503 <Your GitLab email address>
2023-11-13 19:10:52 -0300

    feat: basic logout button

---
+++ src/components/navbar/login-button/logout-button/LogOutButton.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useDispatch } from 'react-redux';
+import { logout } from '../LoginSlice';
+
+const LogOutButton: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const handleLogoutClick = () => {
+    // Clear session storage
+    sessionStorage.clear();
+
+    // Dispatch the logout action
+    dispatch(logout());
+  };
+
+  return (
+    <button className="logout-button" onClick={handleLogoutClick}>
+      {/* You can customize the icon here, for example, using an "x" inside a circle */}
+      &#x2715;
+    </button>
+  );
+};
+
+export default LogOutButton;
--- src/components/navbar/user-display/UserDisplay.tsx
+++ src/components/navbar/user-display/UserDisplay.tsx
@@ -1,20 +1,27 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import {RootState} from '../../../store/store'
+import LogoutButton from '../login-button/logout-button/LogOutButton';
+
 
 const UserDisplay: React.FC = () => {
   const user = useSelector((state:RootState ) => state.login.user);
   console.log('UserDisplay - user:', user); // Add this line for debugging
 
   return (
+    
     <div>
       {user ? (
+        <>
         <p>Hi, {user.email}!</p>
+         <LogoutButton/>
+         </>
       ) : (
         <p>Please log in to see your email.</p>
       )}
     </div>
   );
+  
 };
 
 export default UserDisplay;
