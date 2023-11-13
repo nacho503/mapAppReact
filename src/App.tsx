ab11a14f6176ed93e8af78a0b872b20c52fa509d
nacho503 <Your GitLab email address>
2023-11-13 16:26:01 -0300

    feat: added map and rendered

--- src/App.tsx
+++ src/App.tsx
@@ -2,29 +2,31 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import logo from './logo.svg';
 import Navbar from './components/navbar/Navbar';
-
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import './App.css';
 import './styles/styles.scss';
+import Map from './components/map-container/MapLoader';
+
+
+const render = (status: Status) => {
+  return <h1>{status}</h1>;
+};
+
+const API_KEY = 'AIzaSyBmqsxJV92Ezb_4zFZxkEE6tL20wBPb4lA';
+
+const style = {
+  width: "100vw",
+  height: "90vh",
+  };
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Navbar/>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Wrapper apiKey={API_KEY} render={render}>
+        <Map style={style}/>
+      </Wrapper>
     </div>
     </Router>
   );
